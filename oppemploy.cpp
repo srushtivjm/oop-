1203099f08b9158fcde84ca7e8cda9f05c8ae122
srushtivjm <127848798+srushtivjm@users.noreply.github.com>
2023-04-25 13:43:11 +0530

    Add files via upload

---
+++ oppemploy.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class Employ
+{
+	public:
+	int id;
+	char name[100];
+	char comp[100];
+	int sel;
+	char add[100];
+};
+main()
+{
+	
+	Employ e1,e2,e3,e4,e5;
+	
+	cout<<"Id=";
+	cin>>e1.id;
+	cout<<"Name=";
+	cin>>e1.name;
+	cout<<"Company name=";
+	cin>>e1.comp;
+	cout<<"Selery=";
+	cin>>e1.sel;
+	cout<<"Address=";
+	cin>>e1.add;
+	
+	cout<<"Id=";
+	cin>>e2.id;
+	cout<<"Name=";
+	cin>>e2.name;
+	cout<<"Company name=";
+	cin>>e2.comp;
+	cout<<"Selery=";
+	cin>>e2.sel;
+	cout<<"Address=";
+	cin>>e2.add;
+	
+	cout<<"Id=";
+	cin>>e3.id;
+	cout<<"Name=";
+	cin>>e3.name;
+	cout<<"Company name=";
+	cin>>e3.comp;
+	cout<<"Selery=";
+	cin>>e3.sel;
+	cout<<"Address=";
+	cin>>e3.add;
+	
+	cout<<"Id=";
+	cin>>e4.id;
+	cout<<"Name=";
+	cin>>e4.name;
+	cout<<"Company name=";
+	cin>>e4.comp;
+	cout<<"Selery=";
+	cin>>e4.sel;
+	cout<<"Address=";
+	cin>>e4.add;
+	
+	cout<<"Id=";
+	cin>>e5.id;
+	cout<<"Name=";
+	cin>>e5.name;
+	cout<<"Company name=";
+	cin>>e5.comp;
+	cout<<"Selery=";
+	cin>>e5.sel;
+	cout<<"Address=";
+	cin>>e5.add;
+	
+	cout<<"---------------------------------";
+	
+	cout<<"Id="<<e1.id<<endl
+	<<"Name="<<e1.name<<endl
+	<<"Company name="<<e1.comp<<endl
+	<<"Selery="<<e1.sel<<endl
+	<<"Address="<<e1.add<<endl;
+	
+	cout<<"Id="<<e2.id<<endl
+	<<"Name="<<e2.name<<endl
+	<<"Company name="<<e2.comp<<endl
+	<<"Selery="<<e2.sel<<endl
+	<<"Address="<<e2.add<<endl;
+	
+	cout<<"Id="<<e3.id<<endl
+	<<"Name="<<e3.name<<endl
+	<<"Company name="<<e3.comp<<endl
+	<<"Selery="<<e3.sel<<endl
+	<<"Address="<<e3.add<<endl;
+	
+	cout<<"Id="<<e4.id<<endl
+	<<"Name="<<e4.name<<endl
+	<<"Company name="<<e4.comp<<endl
+	<<"Selery="<<e4.sel<<endl
+	<<"Address="<<e4.add<<endl;
+	
+	cout<<"Id="<<e5.id<<endl
+	<<"Name="<<e5.name<<endl
+	<<"Company name="<<e5.comp<<endl
+	<<"Selery="<<e5.sel<<endl
+	<<"Address="<<e5.add<<endl;
+}
---
+++ oppstudent.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class Student
+{
+	public:
+	int roll_no;
+	char name[100];
+	int std;
+	char cont_no[11];
+	char add[100];
+};
+main()
+{
+	Student s1,s2,s3,s4,s5;
+	
+	cout<<"Roll no=";
+	cin>>s1.roll_no;
+	cout<<"Name=";
+	cin>>s1.name;
+	cout<<"Standred=";
+	cin>>s1.std;
+	cout<<"Contect no=";
+	cin>>s1.cont_no;
+	cout<<"address=";
+	cin>>s1.add;
+	
+	cout<<"Roll no=";
+	cin>>s2.roll_no;
+	cout<<"Name=";
+	cin>>s2.name;
+	cout<<"Standred=";
+	cin>>s2.std;
+	cout<<"Contect no=";
+	cin>>s2.cont_no;
+	cout<<"address=";
+	cin>>s2.add;
+	
+	cout<<"Roll no=";
+	cin>>s3.roll_no;
+	cout<<"Name=";
+	cin>>s3.name;
+	cout<<"Standred=";
+	cin>>s3.std;
+	cout<<"Contect no=";
+	cin>>s3.cont_no;
+	cout<<"address=";
+	cin>>s3.add;
+	
+	cout<<"Roll no=";
+	cin>>s4.roll_no;
+	cout<<"Name=";
+	cin>>s4.name;
+	cout<<"Standred=";
+	cin>>s4.std;
+	cout<<"Contect no=";
+	cin>>s4.cont_no;
+	cout<<"address=";
+	cin>>s4.add;
+	
+	cout<<"Roll no=";
+	cin>>s5.roll_no;
+	cout<<"Name=";
+	cin>>s5.name;
+	cout<<"Standred=";
+	cin>>s5.std;
+	cout<<"Contect no=";
+	cin>>s5.cont_no;
+	cout<<"address=";
+	cin>>s5.add;
+	
+	cout<<"Roll no="<<s3.roll_no<<endl
+	<<"Name="<<s1.name<<endl
+	<<"standred="<<s1.std<<endl
+	<<"Contect no="<<s1.cont_no<<endl
+	<<"Address="<<s1.add<<endl;	
+	
+	cout<<"Roll no="<<s2.roll_no<<endl
+	<<"Name="<<s2.name<<endl
+	<<"standred="<<s2.std<<endl
+	<<"Contect no="<<s2.cont_no<<endl
+	<<"Address="<<s2.add<<endl;
+	
+	cout<<"Roll no="<<s1.roll_no<<endl
+	<<"Name="<<s3.name<<endl
+	<<"standred="<<s3.std<<endl
+	<<"Contect no="<<s3.cont_no<<endl
+	<<"Address="<<s3.add<<endl;	
+	
+	cout<<"Roll no="<<s4.roll_no<<endl
+	<<"Name="<<s4.name<<endl
+	<<"standred="<<s4.std<<endl
+	<<"Contect no="<<s4.cont_no<<endl
+	<<"Address="<<s4.add<<endl;
+	
+	cout<<"Roll no="<<s5.roll_no<<endl
+	<<"Name="<<s5.name<<endl
+	<<"standred="<<s5.std<<endl
+	<<"Contect no="<<s5.cont_no<<endl
+	<<"Address="<<s5.add<<endl;			
+    
+}
